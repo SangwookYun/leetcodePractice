766a34f08b2fa352e4b7f5a527bc259cca2c61af
윤상욱 <sangwookyun@yunsang-ugs-MacBook-Air.local>
2023-01-07 22:22:51 -0800

    study Jan 7

--- binary_tree/binary_tree_postorder_traversal.py
+++ binary_tree/binary_tree_postorder_traversal.py
@@ -1,1 +1,40 @@
-print("hello World!")
+from TreeNode import TreeNode
+
+class Solution:
+    def postorder_traversal_recursive(self, root):
+        ans = []
+        def traversal(root):
+            if not root:
+                return
+            traversal(root.left)
+            traversal(root.right)
+            ans.append(root.val)
+        traversal(root)
+
+        return ans
+
+    def postorder_traversal_iterative(self, root):
+        stack = [root]
+        ans = []
+        while stack:
+            cur = stack.pop()
+            if cur.left:
+                stack.append(cur.left)
+            if cur.right:
+                stack.append(cur.right)
+            ans.append(cur.val)
+        return ans[::-1]
+
+
+if __name__ == "__main__":
+    root1 = TreeNode(1)
+    root2 = TreeNode(2)
+    root3 = TreeNode(3)
+
+    root1.right = root2 
+    root2.left = root3
+    solution = Solution()
+    solution1 = solution.postorder_traversal_recursive(root1)
+    print(solution1)
+    solution2 = solution.postorder_traversal_iterative(root1)
+    print(solution2)
