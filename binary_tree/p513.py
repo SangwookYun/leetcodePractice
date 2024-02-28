be9e3f2425e29fcda01a2b9ae2e5300493e5aa42
Yun <sangwook.yun@sap.com>
2024-02-27 17:08:40 -0800

    STUDY: 20240227

---
+++ binary_tree/p513.py
@@ -0,0 +1,76 @@
+from TreeNode import TreeNode
+class Solution:
+    def findBottomLeftValue(self, root):
+        self.ans = []
+        self.maxDept = 0
+        if root is None:
+            return 0
+        else:
+            self.recursive(root, 0)
+        
+        print(self.ans)
+        return self.ans[-1]
+
+    def recursive(self, node, dept):
+        if node is None:
+            return
+        else:
+            print(node.val, dept, self.maxDept)
+            self.recursive(node.left, dept +1)
+            self.recursive(node.right,dept +1)
+            if (dept>=self.maxDept and len(self.ans) ==0) or (dept > self.maxDept):
+                self.ans.append(node.val)
+                self.maxDept = max(self.maxDept, dept)
+            return           
+        
+if __name__ == "__main__":
+    solution = Solution()
+
+    root1 = TreeNode(2)
+    root2 = TreeNode(1)
+    root3 = TreeNode(3)
+    
+
+    root1.left = root2
+    root1.right = root3
+    
+    #TestCase1
+    print("TESTCASE1")
+    print("PASS" if solution.findBottomLeftValue(root1) == 1 else "FAILED")
+    print("------")
+
+
+    root1 = TreeNode(1)
+    root2 = TreeNode(2)
+    root3 = TreeNode(3)
+    root4 = TreeNode(4)
+    root5 = TreeNode(5)
+    root6 = TreeNode(6)
+    root7 = TreeNode(7)
+    
+
+    root1.left = root2
+    root1.right = root3
+    root2.left = root4
+    root3.left = root5
+    root3.right = root6
+    root5.left = root7
+
+
+    #TestCase2
+    print("TESTCASE2")
+    print("PASS" if solution.findBottomLeftValue(root1) == 7 else "FAILED")
+    print(solution.findBottomLeftValue(root1))
+    print("------")
+
+
+    root1 = TreeNode(0)
+
+
+    #TestCase3
+    print("TESTCASE2")
+    print("PASS" if solution.findBottomLeftValue(root1) == 0 else "FAILED")
+    print(solution.findBottomLeftValue(root1))
+    print("------")
+
+
--- hash/p49.py
+++ hash/p49.py
@@ -1,3 +1,31 @@
 class Solution:
     def groupAnagrams(self, strs):
-        
+        hashmap = {}
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            if sorted_word in hashmap.keys():
+                hashmap[sorted_word].append(word)
+            else:
+                hashmap[sorted_word] = [word]
+            
+        return hashmap.values()
+
+if __name__ == "__main__":
+    solution = Solution()
+    
+    #TestCase1
+    print("TESTCASE1")
+    strs1 = ["eat","tea","tan","ate","nat","bat"]
+    # print("PASS" if solution.groupAnagrams(strs1) == 4 else "FAILED")
+    print(solution.groupAnagrams(strs1))
+    print("------")
+    #TestCase2
+    print("TESTCASE2")
+    strs2 = [""]
+    print(solution.groupAnagrams(strs1))
+    print("------")
+    #TestCase3
+    print("TESTCASE3")
+    strs3 = ["a"]
+    print(solution.groupAnagrams(strs3))
+    print("------")
